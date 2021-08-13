c2d76a083654773dc3ebd2f1ad8591bde89a1473
Anand Mohan <im.anand.m@gmail.com>
2021-08-13 05:48:34 -0700

    created delete mapping to remove user

--- src/main/java/in/codelog/foodbox/controller/UserController.java
+++ src/main/java/in/codelog/foodbox/controller/UserController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,11 @@ public class UserController {
 	@PostMapping("/user")
 	public User createUser(@RequestBody User user) {
 		return userRepository.save(user);
+	}
+
+	@DeleteMapping("/user/{id}")
+	public void deleteUser(@PathVariable(value = "id") int id) {
+		userRepository.deleteById(id);
 	}
 
 }
