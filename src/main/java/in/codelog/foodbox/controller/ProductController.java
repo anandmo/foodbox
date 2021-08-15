b581108eadebc137b8ececf75d77532f582c9532
Anand Mohan <im.anand.m@gmail.com>
2021-08-15 04:21:06 -0700

    Added rest method to update product by id

--- src/main/java/in/codelog/foodbox/controller/ProductController.java
+++ src/main/java/in/codelog/foodbox/controller/ProductController.java
@@ -4,15 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.codelog.foodbox.model.Product;
+import in.codelog.foodbox.model.User;
 import in.codelog.foodbox.repository.ProductRepository;
 
 @RestController
@@ -40,6 +43,22 @@ public class ProductController {
 	@DeleteMapping("/product/{id}")
 	public void removeProduct(@PathVariable(value = "id") int id) {
 		productRepository.deleteById(id);
+	}
+
+	@PutMapping("/product/{id}")
+	public ResponseEntity<Object> updateProduct(@RequestBody Product product, @PathVariable(value = "id") int id) {
+
+		Product tempProduct = productRepository.getById(id);
+
+		if (tempProduct == null) {
+			return ResponseEntity.notFound().build();
+		}
+
+		product.setP_id(id);
+		productRepository.save(product);
+
+		return ResponseEntity.noContent().build();
+
 	}
 
 }
